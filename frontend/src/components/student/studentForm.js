cce3aacd086d5c92192b4695c5464550f9f1f50b
Suhas More <suhas.more@joshsoftware.com>
2019-04-26 22:10:45 +0530

    added student components

---
+++ frontend/src/components/student/studentForm.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const StudentForm = () => <h1>StudentForm  Form</h1>;
+export default StudentForm;
---
+++ frontend/src/components/student/studentList.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const StudentList = () => <h1>Student List</h1>;
+export default StudentList;
