46069c4bbe8a291d61059969f6174bb31d89251d
Swapnil Chincholkar <swapnil@joshsoftware.com>
2019-04-27 02:19:09 +0530

    Added parent crud
    Updated address schema for corrections
    Added json methods as needed

--- backend/app/models/address.rb
+++ backend/app/models/address.rb
@@ -1,4 +1,14 @@
 class Address < ApplicationRecord
+  TYPES = %w{current permanent}
 
   belongs_to :resource, polymorphic: true
+
+  validates :line_1, :city, :state, :pincode, :address_type, presence: true
+  validates :pincode, numericality: { only_integer: true }
+
+  def as_json(options = {})
+    super(only: [:id, :line_1, :line_2, :area, :landmark, :city, :state,
+      :pincode, :resource_id, :resource_type, :address_type]
+    )
+  end
 end
