18954204d788dc919cd330f9de8be0810ae9379c
Suhas More <suhas.more@joshsoftware.com>
2019-04-27 03:59:18 +0530

    added standerds and class list view

--- frontend/src/components/class/classList.js
+++ frontend/src/components/class/classList.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Empty } from 'antd';
+import { Empty, Row, Col } from 'antd';
+import { connect } from 'react-redux';
+import { getClassList } from '../../actions/appActions/classActions';
 import ClassModel from '../../models/AppModel/ClassModel';
+
 import './class.scss';
+import ClassCard from './ClassCard';
+import routes from '../../utils/routes';
+import KeyListener from '../helpers/KeyListner';
 
 class ClassList extends React.Component {
   constructor(props) {
@@ -12,24 +18,92 @@ class ClassList extends React.Component {
   }
 
   componentDidMount() {
-    // API call
+    this.getClassListAPI();
+  }
+
+  getClassListAPI = () => {
+    // getClassList()
+    // .then()ta
+    // .catch()
+
+    const classes = [
+      {
+        id: '10-123-123-',
+        standerd: 10,
+        division: 'A',
+        totalStudents: 57,
+      },
+      {
+        id: '123-123-123-',
+        standerd: 10,
+        division: 'B',
+        totalStudents: 57,
+      },
+      {
+        id: '12-123-123-',
+        standerd: 10,
+        division: 'C',
+        totalStudents: 57,
+      },
+    ];
+    ClassModel.saveAll(classes.map(classss => new ClassModel(classss)));
+  }
+
+  handleTakePresentyClick = (id) => {
+    console.log(id);
+  }
+
+  handleViewClassClick = (id) => {
+    const { history: { push } } = this.props;
+    push(`${routes.classList}/${id}`);
+  }
+
+  getClassCard = (payload) => {
+    return payload.map(data => (
+      <Col lg={{ span: 7, offset: 1 }} sm={{ span: 10, offset: 1 }} md={{ span: 10, offset: 1 }}>
+        <ClassCard
+          data={data}
+          handleViewClick={() => this.handleViewClassClick(data.id)}
+          handlePresenty={() => this.handleTakePresentyClick(data.id)}
+        />
+      </Col>
+    ));
   }
 
   getClassList = () => {
     const { classes } = this.props;
-    if (!classes || classes.length) {
+    if (!classes || classes.length === 0) {
       return (
         <Empty description="No classes found"/>
       );
     }
+    return (
+      <Row>
+        {this.getClassCard(classes)}
+      </Row>
+    );
+  }
+
+  handleAddClick = () => {
+    const { history: { push }} = this.props;
+    push(routes.classAdd);
   }
 
   render() {
     return (
-      <div className="class-list-container">
-        {this.getClassList()}
-      </div>
+      <KeyListener onNew={this.handleAddClick}>
+        <div className="class-list-container">
+          {this.getClassList()}
+        </div>
+      </KeyListener>
     );
   }
 }
-export default ClassList;
+
+function mapStateToProps() {
+  return {
+    classes: ClassModel.list()[0] ? ClassModel.list().map(item => item[1].props) : []
+  };
+}
+
+export default connect(mapStateToProps)(ClassList);
