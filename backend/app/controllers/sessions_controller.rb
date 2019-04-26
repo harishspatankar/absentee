1d83f7466885a3f83c9f144df8a7139a4227ef8c
Ashvini <ashvini@joshsoftware.com>
2019-04-26 22:38:44 +0530

    Added signin and sign out actions.

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+  # skip_before_action :validate_jwt_token!, only: [:create]
+  # skip_before_action :authenticate!, only: [:create ]
+
+  def create
+    head :bad_request and return false unless params[:user].present?
+    user = User.where(mobile_number: params[:user][:mobile_number]).first
+    if user && user.valid_password?(params[:user][:password])
+      render json: {messasge: "Signed in successful", user: user.as_json(methods: [:api_key])}
+    else
+      render json: { message: "Authentication failed" }, status: :unauthorized
+    end
+  end
+
+
+  def destroy
+    # head :bad_request and return false unless params[:user].present?
+    if sign_out current_user
+      render json: { message: "Signed out successfully." }
+    else
+      render json: { message: "Failed to sign out." }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def validate_jwt_token!
+    jwt_payload
+  rescue JWT::DecodeError
+    head :unauthorized
+    return false
+  end
+
+  def jwt_payload
+    @jwt_payload ||= JWT.decode(request.headers[X_API_KEY], JWT_SECRET)
+  end
+end
