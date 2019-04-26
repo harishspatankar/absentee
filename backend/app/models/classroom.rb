8ec1faec1173dae73671eac5ffa6ee0e9c69c41c
Ashvini <ashvini@joshsoftware.com>
2019-04-27 02:05:56 +0530

    Added classroom CRUD and routes.

--- backend/app/models/classroom.rb
+++ backend/app/models/classroom.rb
@@ -1,3 +1,11 @@
 class Classroom < ApplicationRecord
   validates :standard, :start_time, :end_time, presence: true
+  # validate :end_time, :numericality => { :greater_than => true }
+  validate :start_time_cannot_be_greater_than_end_time
+
+  def start_time_cannot_be_greater_than_end_time
+    if start_time >= end_time
+      errors.add(:start_time, "cannot be greater than end time.")
+    end
+  end
 end
