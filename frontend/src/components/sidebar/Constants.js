3e87013527178c43d6ff267b12b207337b4d71ce
Suhas More <suhas.more@joshsoftware.com>
2019-04-26 22:10:33 +0530

    added new menus

--- frontend/src/components/sidebar/Constants.js
+++ frontend/src/components/sidebar/Constants.js
@@ -1,18 +1,18 @@
 import routes from '../../utils/routes';
 
 const NAV_MENU = [
-  {
-    name: 'Teachers',
-    path: routes.teachers,
-    icon: 'file-search',
-    permission: ['2', '4', '8'],
-  },
-  {
-    name: 'Classes',
-    path: routes.dashboard,
-    icon: 'fund',
-    permission: ['2', '4', '8'],
-  },
+    {
+        name: 'Teachers',
+        path: routes.teachers,
+        icon: 'file-search',
+        permission: ['2', '4', '8'],
+    },
+    {
+        name: 'Classes',
+        path: routes.classList,
+        icon: 'fund',
+        permission: ['2', '4', '8'],
+    },
 ];
 
 export default NAV_MENU;
--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import {
-  Layout, Menu, Icon, Divider,
+    Layout, Menu, Icon, Divider,
 } from 'antd';
 import NAV_MENU from './Constants';
 import './Sidebar.scss';
@@ -12,46 +12,46 @@ import { setItem, getItem } from '../helpers/localStorage';
 
 const { Sider } = Layout;
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    const { open } = props;
-    this.state = {
-      open,
-      activeMenu: routes.targetGroupList,
-    };
-    props.history.push(routes.targetGroupList);
-  }
+    constructor(props) {
+        super(props);
+        const { open } = props;
+        this.state = {
+            open,
+            activeMenu: routes.targetGroupList,
+        };
+        props.history.push(routes.targetGroupList);
+    }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      open: nextProps.open,
-    });
-  }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            open: nextProps.open,
+        });
+    }
 
   handleMenuChange = ({ key }) => {
-    if (key.includes('log-out')) {
-      setItem('token', '');
-      this.props.history.push(routes.root);
-      return 0;
-    }
-    this.setState({
-      activeMenu: key,
-    });
-    this.props.history.push(key);
+      if (key.includes('log-out')) {
+          setItem('token', '');
+          this.props.history.push(routes.root);
+          return 0;
+      }
+      this.setState({
+          activeMenu: key,
+      });
+      this.props.history.push(key);
   }
 
   getMenu = () => {
-    const profile = getItem('profile');
-    return NAV_MENU.map(({ path, name, icon, permission }) => {
-      if (true) {
-        return (
-          <Menu.Item key={`${path}`}>
+      const profile = getItem('profile');
+      return NAV_MENU.map(({ path, name, icon, permission }) => {
+          if (true) {
+              return (
+                <Menu.Item key={`${path}`}>
             <Icon type={icon} />
             <span>{name}</span>
           </Menu.Item>
-        );
-      }
-    });
+              );
+          }
+      });
   }
 
   render() {
@@ -65,15 +65,15 @@ class Sidebar extends Component {
         <div className="sidebar-logo"> Demo App </div>
         <Divider />
         <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={[activeMenu]}
-          onClick={this.handleMenuChange}
+            theme="dark"
+            mode="inline"
+            selectedKeys={[activeMenu]}
+            onClick={this.handleMenuChange}
         >
           {this.getMenu()}
         </Menu>
       </Sider>
-    );
+      );
   }
 }
 export default withRouter(Sidebar);
