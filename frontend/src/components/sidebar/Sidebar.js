578789ab38d0b7b3b82a587e2ddbaecffc70b7d6
Suhas More <suhas.more@joshsoftware.com>
2019-04-27 10:49:42 +0530

    added language change ui in sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -3,58 +3,75 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import {
-    Layout, Menu, Icon, Divider, Dropdown, Avatar,
+  Layout, Menu, Icon, Divider, Dropdown, Avatar, Switch,
 } from 'antd';
+import LocalizedStrings from 'react-localization';
 import NAV_MENU from './Constants';
 import './Sidebar.scss';
 import routes from '../../utils/routes';
 import { setItem, getItem } from '../helpers/localStorage';
 
+const strigs = new LocalizedStrings({
+  EN: {
+    appName: 'Absentee',
+  },
+  MR: {
+    appName: 'हजेरीपट',
+  },
+});
+
 const { Sider } = Layout;
 class Sidebar extends Component {
-    constructor(props) {
-        super(props);
-        const { open } = props;
-        this.state = {
-            open,
-            activeMenu: routes.targetGroupList,
-        };
-        props.history.push(routes.targetGroupList);
-    }
+  constructor(props) {
+    super(props);
+    const { open } = props;
+    this.state = {
+      open,
+      activeMenu: routes.targetGroupList,
+    };
+    props.history.push(routes.targetGroupList);
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            open: nextProps.open,
-        });
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      open: nextProps.open,
+    });
+  }
 
   handleMenuChange = ({ key }) => {
-      if (key.includes('log-out')) {
-          setItem('token', '');
-          this.props.history.push(routes.root);
-          return 0;
-      }
-      this.setState({
-          activeMenu: key,
-      });
-      this.props.history.push(key);
+    if (key.includes('log-out')) {
+      setItem('token', '');
+      this.props.history.push(routes.root);
+      return 0;
+    }
+    this.setState({
+      activeMenu: key,
+    });
+    this.props.history.push(key);
   }
 
   getMenu = () => {
-      const profile = getItem('profile');
-      return NAV_MENU.map(({ path, name, icon, permission }) => {
-        if (true) {
+    const profile = getItem('profile');
+    return NAV_MENU.map(({ path, name, icon, permission }) => {
+      if (true) {
         return (
-            <Menu.Item key={`${path}`}>
-            <Icon type={icon} />
-            <span>{name}</span>
-            </Menu.Item>
-            );
-        }
-      });
+          <Menu.Item key={`${path}`}>
+          <Icon type={icon} />
+          <span>{name}</span>
+          </Menu.Item>
+        );
+      }
+    });
   }
 
-  
+  setLanguage = (checked) => {
+    const language = checked ? 'EN' : 'MR';
+    this.setState({
+      language,
+    });
+    setItem('language', language);
+    this.props.history.push(window.location);
+  }
 
   getAppAndUserName = () => {
     const menu = (
@@ -72,7 +89,7 @@ class Sidebar extends Component {
     );
     return (
       <div className="user-profile">
-        <div className="sidebar-logo"> Absentee </div>
+        <div className="sidebar-logo">{strigs.appName}</div>
         <div className="avatar"><Avatar shape="square" icon="user"/></div>
         <div className="user-name">
           <Dropdown overlay={menu}>
@@ -81,31 +98,33 @@ class Sidebar extends Component {
             </a>
           </Dropdown>
         </div>
+        <div className="language">
+          <Switch unCheckedChildren="MR" checkedChildren="EN" checked={this.state.language === 'EN'} onChange={this.setLanguage} />
+        </div>
       </div>
     );
   }
 
   render() {
-    const { open, activeMenu } = this.state;
+    const { open, activeMenu, language } = this.state;
+    strigs.setLanguage(getItem('language'));
     return (
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
-        onBreakpoint={(broken) => { console.log(broken); }}
-        onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
       >
         {this.getAppAndUserName()}
         <Divider />
         <Menu
-            theme="dark"
-            mode="inline"
-            selectedKeys={[activeMenu]}
-            onClick={this.handleMenuChange}
+          theme="dark"
+          mode="inline"
+          selectedKeys={[activeMenu]}
+          onClick={this.handleMenuChange}
         >
           {this.getMenu()}
         </Menu>
       </Sider>
-      );
+    );
   }
 }
 export default withRouter(Sidebar);
